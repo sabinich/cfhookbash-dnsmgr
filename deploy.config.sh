57abebaaba6bc1839e55cf40fe357e04b395a9d8
vadim s. sabinich <1430016+sabinich@users.noreply.github.com>
2019-10-08 10:18:29 +0800

    Update deploy.config.sh

--- deploy.config.sh
+++ deploy.config.sh
@@ -4,24 +4,24 @@ case ${1} in
         "www.example.com")
         #move certificate to private
 
-        cp /home/sineverba/dehydrated/certs/domain/cert.pem /var/www/domain/private/cert.pem
-        cp /home/sineverba/dehydrated/certs/domain/privkey.pem /var/www/domain/private/privkey.pem
-        cp /home/sineverba/dehydrated/certs/domain/fullchain.pem /var/www/domain/private/fullchain.pem
+        #cp /home/sineverba/dehydrated/certs/domain/cert.pem /var/www/domain/private/cert.pem
+        #cp /home/sineverba/dehydrated/certs/domain/privkey.pem /var/www/domain/private/privkey.pem
+        #cp /home/sineverba/dehydrated/certs/domain/fullchain.pem /var/www/domain/private/fullchain.pem
 
         #restart nginx
-        service nginx restart
-
+        #service nginx restart
+	systemctl reload nginx
         #exchange certificate for domoticz
-        rm /home/sineverba/domoticz/server_cert.pem
-        cat /var/www/domain/private/privkey.pem >> /home/sineverba/domoticz/server_cert.pem
-        cat /var/www/domain/private/fullchain.pem >> /home/sineverba/domoticz/server_cert.pem
-        cp /home/sineverba/domoticz/server_cert.pem /home/sineverba/domoticz/letsencrypt_server_cert.pem
+        #rm /home/sineverba/domoticz/server_cert.pem
+        #cat /var/www/domain/private/privkey.pem >> /home/sineverba/domoticz/server_cert.pem
+        #cat /var/www/domain/private/fullchain.pem >> /home/sineverba/domoticz/server_cert.pem
+        #cp /home/sineverba/domoticz/server_cert.pem /home/sineverba/domoticz/letsencrypt_server_cert.pem
 
         #restart domoticz
-        /etc/init.d/domoticz.sh restart
+        #/etc/init.d/domoticz.sh restart
 
 	#restart mysensors
-	service mysgw restart
+	#service mysgw restart
 
         ;;
 
